013520cf090ce2492dbf2d73e98c87c409cdf4d3
Mohamed Hamed <mohameddardeery@gmail.com>
2025-02-24 06:41:30 +0200

    Auto-generated commit

--- server/main.server.mjs
+++ server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-ZBMMO7WH.mjs";import"./chunk-N6755FRU.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-X3JYPYNS.mjs";import"./chunk-N6755FRU.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
